9d75c664d9565699039f614cb51a6da58c9c5ba6
samuelbarata <samuel.barata@tecnic.ulisboa.pt>
2020-04-02 20:36:37 +0100

    THEY TALK! como deve de ser

--- client.py
+++ client.py
@@ -7,17 +7,75 @@ server = -1
 MSG_SIZE = 1024
 HLINE="{}{}{}{}{}{}{}{}{}{}{}\n".format(chr(9472), chr(9472), chr(9472),chr(9532),chr(9472), chr(9472),
                                       chr(9472),chr(9532), chr(9472), chr(9472), chr(9472))
+username=''
+
+#mensagens erro:
+ERR                 = "ERROR "
+REG_FAIL            = "Username already in use, pick a different username"
+NO_USER             = "No such user exists, LIST all users and pick a valid user"
+USER_BUSY           = "User is busy, LIST all users to check their status"
+INVALID_COOR        = "Invalid Coordinates, valid {0,1,2}"
+INVALID_PLAY        = "{} already has {}"
+USER_DISCONECTED    = "Oponent has left the game, you win"
+NOT_IN_GAME         = "This command is only valid during a game type HELP to see how to start a new game"
+SERVER_OFF          = "The server will exit and you'll be disconnected automatically"
+BAD_REQUEST         = "Bad Request, type HELP to see available commands"
+USER_UNKNOWN        = "User unregistered, you need to register before doing this action"
+USER_REGISTERED     = "You are already registered"
+
+#mensagens ok:
+SUC                 = "OK "
+REG_OK              = "Registado com sucesso"
+INVITE_OK           = "Waiting for reply..."
+ACEPT               = "{} has acepted your request"
+REJECT              = "{} has rejected your request"
+WAITING_FOR_PLAY    = "Not your turn to play"
+WIN                 = "Congratulations, you have won"
+LOSE                = "Better luck next time"
+TIE                 = "Tie"
+
 
 def exit_sig(sig=0, frame=0):
     client.close()
     exit(0)
+#----
 
 def process_input(server_msg):
     """
     Recieves the server Message, returned value will be sent to server
-    """ 
-    server_msg=str(server_msg).split(" ")
-    if(server_msg[0]=="BOARD"):
+    """
+    global username
+    server_msg=server_msg[:-1:]         #retira o \n
+    original=server_msg
+    server_msg=server_msg.split(" ")    #separa por args
+
+    if(server_msg[0]=="DISPLAY"):
+        for i in range(1,len(server_msg)):
+            print(server_msg[i] + " ", end="")
+        print("\n")
+
+
+
+    elif(server_msg[0]=="SUC"):
+        if(server_msg[1]=="REG_OK"):
+            username=server_msg[2]
+            print(REG_OK)
+        if(server_msg[1]=='INVITE_OK'):
+            print(INVITE_OK)
+
+
+
+    elif(server_msg[0]=="ERR"):
+        if(server_msg[1]=="BAD_REQUEST"):
+            print(BAD_REQUEST)
+        elif(server_msg[1]=="REG_FAIL"):
+            print(REG_FAIL)
+        elif(server_msg[1]=="USER_REGISTERED"):
+            print(USER_REGISTERED)
+
+
+
+    elif(server_msg[0]=="BOARD"):
         board=eval(server_msg[1])
         for i in range(len(board)):
             for k in range(len(board[i])):
@@ -33,6 +91,22 @@ def process_input(server_msg):
             +" {} | {} | {} \n".format(board[1][0], board[1][1], board[1][2])\
             +HLINE\
             +" {} | {} | {} \n".format(board[2][0], board[2][1], board[2][2]))
+
+    elif(server_msg[0]=="LIST"):
+        all_users=eval(original[5::])
+        print("USER\t|\tSTATUS")
+        for i in all_users:
+            print("{}\t|\t{}".format(i[0], "available" if i[1] == 0 else "unavailable"))
+
+
+    elif(server_msg[0]=="SERVER_OFF"):
+        print('\n' + SERVER_OFF + '\n\n')
+        exit_sig()
+    sys.stdout.flush()
+#----
+
+
+
 while(not 0<=server<len(server_ip)):
     server = int(input("Select server:\n0: Remote Server\n1: Localhost\n2: VM\n"))
 
@@ -47,9 +121,10 @@ except:
 
 signal.signal(signal.SIGINT, exit_sig)
 inputs = [client, sys.stdin]
-
+message=''
+print(" COMMAND: ",  end='')
+sys.stdout.flush()
 while True:
-    print("COMAND:")
     ins, outs, exs = select.select(inputs, [], [])
     for i in ins:
         if(i==sys.stdin):
@@ -57,7 +132,13 @@ while True:
             client.sendto(response.encode(),(server_ip[server],server_port))
         elif(i==client):
             (server_msg, addr) = client.recvfrom(MSG_SIZE)
-            process_input(server_msg.decode())
-
-
+            if(not server_msg):
+                exit_sig()
+            message+=str(server_msg.decode())
+            if(message[-1]!='\n'):
+                continue
+            process_input(message)
+            message=''
+            print("{} COMMAND: ".format(username), end='')
+            sys.stdout.flush()
 
--- server.py
+++ server.py
@@ -4,9 +4,6 @@ import socket, sys, threading, signal, os
 bind_address    = ''
 bind_port       = 45080
 
-
-
-
 #   Comandos:
 #       HELP                -- lista os comandos a usar
 #       REGISTER <nome>     -- regista um novo user
@@ -20,60 +17,135 @@ bind_port       = 45080
 
 #mensagens erro:
 ERR                 = "ERROR "
-REG_FAIL            = "Username already in use, pick a different one"
-NO_USER             = "No such user exists, LIST all users and pick a valid user"
-USER_BUSY           = "User is busy, LIST all users to check their status"
-INVALID_COOR        = "Invalid Coordinates, valid {0,1,2}"
-INVALID_PLAY        = "{} already has {}"
-USER_DISCONECTED    = "Oponent has left the game, you win"
-NOT_IN_GAME         = "This command is only valid during a game type HELP to see how to start a new game"
-SERVER_OFF          = "The server will exit and you'll be disconnected automatically"
+REG_FAIL            = "Username already in use, pick a different one\n"
+NO_USER             = "No such user exists, LIST all users and pick a valid user\n"
+USER_BUSY           = "User is busy, LIST all users to check their status\n"
+INVALID_COOR        = "Invalid Coordinates, valid {0,1,2}\n"
+INVALID_PLAY        = "{} already has {}\n"
+USER_DISCONECTED    = "Oponent has left the game, you win\n"
+NOT_IN_GAME         = "This command is only valid during a game type HELP to see how to start a new game\n"
+SERVER_OFF          = "The server will exit and you'll be disconnected automatically\n"
+BAD_REQUEST         = "Bad Request, type HELP to see available commands\n"
+USER_UNKNOWN        = "User unregistered, you need to register before doing this action\n"
+USER_REGISTERED     = "You are already registered\n"
 
 #mensagens ok:
 SUC                 = "OK "
-REG_OK              = "Registado com sucesso"
-INVITE_OK           = "Waiting for reply..."
-ACEPT               = "{} has acepted your request"
-REJECT              = "{} has rejected your request"
-WAITING_FOR_PLAY    = "Not your turn to play"
-WIN                 = "Congratulations, you have won"
-LOSE                = "Better luck next time"
-TIE                 = "Tie"
+REG_OK              = "Registado com sucesso\n"
+INVITE_OK           = "Waiting for reply...\n"
+ACEPT               = "{} has acepted your request\n"
+REJECT              = "{} has rejected your request\n"
+WAITING_FOR_PLAY    = "Not your turn to play\n"
+WIN                 = "Congratulations, you have won\n"
+LOSE                = "Better luck next time\n"
+TIE                 = "Tie\n"
+DISPLAY             = "DISPLAY "
 
-users ={} #user: [socketfd, status]
+users ={} #user: [socketfd, status, [game]]
 server = 0
+connections=[]
 
+def help():
+    return "DISPLAY insert help text here\n"
+#----
 
 def exit_server(sig, frame):
-    print("\nEXITing server recieved signal {}, on {}".format(sig, frame))
+    print("\nExiting server recieved signal {}, on {}...".format(sig, frame))
     server.close()
-    for user in users:
-        users[user][0].send(SERVER_OFF.encode())
-        socket.close(users[user][0])
+    for c in connections:
+        c.send("SERVER_OFF\n".encode())
+        c.close()
     exit(0)
+#----
 
-def process_input(msg):
-    #return "OLA"
-    return "BOARD [[0,0,0],[0,1,2],[0,0,0]]"
+def register(user, client):
+    for i in users:
+        if(users[i][0]==client):
+            return "ERR USER_REGISTERED\n"
+    users[user]=[client, 0]
+    return "SUC REG_OK " +user + "\n"
+#----
 
+def list():
+    return "LIST " + str([(i, users[i][1]) for i in users]) + '\n'
+#----
+
+def invite(myself, user):
+    for i in users:
+        if(users[i][0] == myself):
+            myself=users[i]
+            break
+    if(myself not in users):
+        return ERR + USER_UNKNOWN
+    if(user not in users):
+        return ERR + NO_USER
+    if(users[user][1]!=0):
+        return ERR + USER_BUSY
+    users[myself][1]=1
+    users[user][0].send("INVITE {}".format(myself).encode())
+    return "SUC INVITE_OK\n"
+#----
+
+def process_input(msg, client):
+    msg=msg[:-1:] #retira o \n
+    msg=msg.upper()
+    msg=msg.split(" ")
+    print("Recieved >{}<".format(msg))
+
+    if(msg[0]=="HELP"):
+        return help()
+    elif(msg[0]=="REGISTER"):
+        if(len(msg)==2):
+            return register(msg[1], client)
+        else:
+            return ERR + BAD_REQUEST
+    elif(msg[0]=="LIST"):
+        if(len(msg)==1):
+            return list()
+        else:
+            return ERR + BAD_REQUEST
+    elif(msg[0]=="EXIT"):
+        raise socket.timeout()
+
+    elif(msg[0]=="INVITE"):
+        if(len(msg)!=2):
+            pass
+        else:
+            return invite(msg[1])
+
+ 
+    return "ERR BAD_REQUEST\n"
+#----
 
 
 
 def handle_client_connection(client_sock):
     signal.pthread_sigmask(signal.SIG_SETMASK, [signal.SIGINT, signal.SIGKILL])
     try:
+        connections.append(client_sock)
         while True:
             request=''
             #soma strings ate ter mensagem completa
             while(request=='' or request[-1]!='\n'):
                 msg_from_client = client_sock.recv(1024)
-                request += msg_from_client.decode()
-            client_sock.send(process_input(request).encode())
-    except:
+                if(not msg_from_client):
+                    raise socket.timeout()
+                request += str(msg_from_client.decode())
+                
+            client_sock.send(process_input(request, client_sock).encode())
+    except socket.timeout:
+        for i in users:
+            if(users[i][0]==client_sock):
+                del(users[i])
+                break
+        for i in range(len(connections)):
+            if(connections[i]==client_sock):
+                del(connections[i])
+                break
         client_sock.close()
         print("Client disconected".format(client_sock))
         exit(0)
-
+#----
 
 
 
@@ -90,6 +162,7 @@ def main():
         print("Acepted connection from {}:{}".format(address[0],address[1]))
         client_handler=threading.Thread(target=handle_client_connection, args=(client_sock, ))
         client_handler.start()
+#----
 
 if(__name__ == '__main__'):
     main()
